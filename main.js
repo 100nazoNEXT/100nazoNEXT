9990402d025db81fdea9e1ce19f26dd581243211
100nazoNEXT <111350301+100nazoNEXT@users.noreply.github.com>
2022-08-17 18:04:36 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 var btn = document.getElementById('btn');
  
     btn.addEventListener('click', function() {
-      console.log("解答ありがとう！");
+      
     })
+
+    var val = document.form.input.value;
+            var target = document.getElementById("output");
+
+            if (val == "はじまり||ハジマリ||始まり") {
+                target.innerHTML = "合っています。";
+            } else {
+                target.innerHTML = "間違っています。";
+            }
