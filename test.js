a68ba5b20081d2fda72a07e5bf5d8ac05e89a005
100nazoNEXT <111350301+100nazoNEXT@users.noreply.github.com>
2023-01-02 15:00:03 +0900

    Add files via upload

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 let namae = ['アリス1', 'アリス2', 'アリス3','アリス4','アリス5','アリス6','アリス7','アリス8']
-let at = ['arith_nazotoki','arith_nazotoki','arith_nazotoki','arith_nazotoki','arith_sub','arith_nazotoki','','']
+let at = ['arith_nazotoki','arith_nazotoki','arith_nazotoki','arith_nazotoki','arith_nazotoki','arith_nazotoki','','']
+let ex = ['4つの単語をそれぞれ「め欠け」「な無し」「し無い」「け無視」と捉え、「めかけななししないけむし」から「め」「な」「し」「け」を全て消すことで「かいむ」となります。よって正解は『皆無』です。','','','','','','傾いている大きいアルファベットが壁の迷路と捉え、S(スタート)からG(ゴール)まで進むと「KOTAE HA MEIRO」を通ります。よって答えは『迷路』でした！','']
+                        
 
 function arrayShuffle(array) {
     for(var i = (array.length - 1); 0 < i; i--){
@@ -28,6 +30,23 @@ function counter(){
     document.getElementById("gazou").src=jp;
     jp2 = nazo[num = numbers[nazovotecnt + 1]];
     document.getElementById("gazou2").src=jp2;
+    chwer = naz[suuji = numbers[nazovotecnt]];
+    document.getElementById("werch").src=chwer;
+    chwer2 = naz2[suuji = numbers[nazovotecnt + 1]];
+    document.getElementById("werch2").src=chwer2;
+    document.getElementById("vi").innerHTML = prex();
+        document.getElementById("vi2").innerHTML = prex2();
+    if (numbers[nazovotecnt] == 0 || numbers[nazovotecnt] == 6) {
+        document.getElementById("vis").style.display="block";
+        document.getElementById("visi").style.display="none";
+        document.getElementById("vis2").style.display="block";
+        document.getElementById("visi2").style.display="none";
+    } else {
+        document.getElementById("vis").style.display="none";
+        document.getElementById("visi").style.display="block";
+        document.getElementById("vis2").style.display="none";
+        document.getElementById("visi2").style.display="block";
+    }
     if (numbers[nazovotecnt] == 6 || numbers[nazovotecnt] == 7) {
         document.getElementById("toumei").style.visibility="hidden";
     } else {
@@ -58,4 +77,14 @@ function prat(){
 function prat2(){
     let num = numbers[nazovotecnt + 1];
     return at[num];
+}
+
+function prex(){
+    let num = numbers[nazovotecnt];
+    return ex[num];
+}
+
+function prex2(){
+    let num = numbers[nazovotecnt + 1];
+    return ex[num];
 }
